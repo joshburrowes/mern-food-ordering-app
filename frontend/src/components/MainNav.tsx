66b10d914fc40215cde6568ce4c2a2f088ac1f49
joshburrowes <burrowes.joshua@gmail.com>
2024-09-21 00:10:12 +0100

    fix name

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-import UserNameMenu from "./UserNameMenu";
+import UserNameMenu from "./UsernameMenu";
 
 export default function MainNav() {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
