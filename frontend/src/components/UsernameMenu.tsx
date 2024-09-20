066120597299a773cedda3f78938d62f5f3558c2
joshburrowes <burrowes.joshua@gmail.com>
2024-09-20 23:43:59 +0100

    add user name dropdown

---
+++ frontend/src/components/UsernameMenu.tsx
@@ -0,0 +1,32 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { CircleUserRound } from "lucide-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+
+const UserNameMenu = () => {
+  const { user } = useAuth0();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover: text-orange-500 gap-2">
+        <CircleUserRound className="text-orange-500" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold text-orange-500">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserNameMenu;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './global.css'
-import AppRoutes from './AppRoutes'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import AppRoutes from "./AppRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes/>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
