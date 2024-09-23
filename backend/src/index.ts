43cbed26e44f0c9296e6aac222accb04b1fd83a6
joshburrowes <burrowes.joshua@gmail.com>
2024-09-23 22:16:15 +0100

    add login/logout functionality for mobile nav, start implementing user api to save users to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,19 @@
 import express, { Request, Response } from "express";
-import cors from 'cors';
+import cors from "cors";
 import "dotenv/config";
-import exp from "constants";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-.then(()=> console.log("Connected to database."));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database."));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response)=> {
-    res.json({message: "Hello World"})
-});
+app.use("/api/my/user", myUserRoute);
 
-app.listen(7000, ()=>{
-    console.log("server started on localhost:7000")
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
 });
--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -8,9 +8,10 @@ import { CircleUserRound } from "lucide-react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const UserNameMenu = () => {
-  const { user } = useAuth0();
+  const { user, logout } = useAuth0();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center px-3 font-bold hover: text-orange-500 gap-2">
@@ -24,6 +25,14 @@ const UserNameMenu = () => {
           </Link>
         </DropdownMenuItem>
         <Separator />
+        <DropdownMenuItem>
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
