0a6d67dfb1f3394c0749e95d2fc3f5718573f107
joshburrowes <burrowes.joshua@gmail.com>
2024-11-06 00:18:21 +0000

    redirect callback separate page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Layout from "./layouts/layout";
+import Layout from "./layouts/Layout";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
           </Layout>
         }
       ></Route>
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route
         path="/user-profile"
         element={<span>USER PROFILE PAGE</span>}
